b95120bdf56c24f20340208d1869e22d9454f8a7
Yuvraj <yuvrajsharma21314@gmail.com>
2026-05-19 22:18:18 +0530

    Version

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*html"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#0A0A0A",
+        secondry: "#F8F8FF",
+      },
+    },
+  },
+  plugins: [],
+};
